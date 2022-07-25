32db30820f196385859989595238ed1d0a368e34
Jakob <JacobADoney@gmail.com>
2022-07-25 15:32:21 -0500

    Add files via upload

---
+++ Stack II/data.h
@@ -0,0 +1,17 @@
+/*
+ * COSC 2436
+ * Stacks Assignment
+ */
+
+#ifndef STACKS_DATA_H
+#define STACKS_DATA_H
+
+#include <string>
+using std::string;
+
+struct Data {
+    int id;
+    string information;
+};
+
+#endif /* STACKS_DATA_H */
